fb4df985a195c31cb0bae1623e21091c0baf7b20
Lawrence Moore <lmoore@idauto.net>
2018-01-27 13:17:08 -0500

    Added login service

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
 const customers = require('./companies/companies.service.js');
 const reviews = require('./reviews/reviews.service.js');
+const login = require('./login/login.service.js');
 
 module.exports = function () {
     const app = this;
     app.configure(reviews);
     app.configure(customers);
+    app.configure(login);
 };
---
+++ src/services/login/login.service.js
@@ -0,0 +1,28 @@
+const request = require('request-promise');
+
+module.exports = function() {
+    const app = this;
+
+    const loginUser = {
+        create(body) {
+            const makeRequest = request.defaults({
+                method: 'POST',
+                url: app.baseUrl + 'v1/applications/' + app.apiID + '/session',
+                body: {
+                    email: body.email,
+                    password: body.password
+                },
+                json: true
+            });
+
+            return makeRequest().then(function (parsedBody) {
+                return parsedBody;
+            })
+                .catch(function (err) {
+                    return err;
+                });
+        }
+    };
+
+    app.use('/login', loginUser);
+};
